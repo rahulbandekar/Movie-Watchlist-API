fb678328c98a8d6d85dda026462f5b08b4bcb91a
rahulbandekar <rahulbandekar12@gmail.com>
2026-04-22 21:36:12 +0530

    change cookie samesite

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    sameSite: isProduction ? "lax" : "none",
     maxAge: 1000 * 60 * 60 * 24 * 7,
   });
   
