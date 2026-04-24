a9f85317453e893167b757581dbe857ee4520e8a
rahulbandekar <rahulbandekar12@gmail.com>
2026-04-24 09:11:41 +0530

    Hardcode expiresIn to 7d for testing

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) => {
   const payload = { id: userId };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    // expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    expiresIn: "7d",
   });
 
 
